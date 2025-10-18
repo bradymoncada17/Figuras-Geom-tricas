7c0dcc7faa756be268cef033f110489a7aac4d82
bradymoncada17 <bradymoncada17@gmail.com>
2025-10-18 12:07:59 -0500

    Herencia, polimorfismo, metodos y lógica de las figuras

--- Figuras Geométricas/Figura.cs
+++ Figuras Geométricas/Figura.cs
@@ -8,6 +8,13 @@ namespace Figuras_Geométricas
 {
     public abstract class Figura
     {
+        public Color Color { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Tamaño { get; set; } // usado para rectángulo/círculo/triángulo
+        public abstract void Dibujar(Graphics g);
+    }
+
 
 
     }
