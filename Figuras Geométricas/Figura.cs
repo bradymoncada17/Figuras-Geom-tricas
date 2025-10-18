396609693fc0fc806c5f5cebee5fb1fa0e68d592
bradymoncada17 <bradymoncada17@gmail.com>
2025-10-18 11:42:56 -0500

    Creación de Clase y subclases

---
+++ Figuras Geométricas/Figura.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figuras_Geométricas
+{
+    public abstract class Figura
+    {
+
+
+    }
+
+
+
+
+
+}
